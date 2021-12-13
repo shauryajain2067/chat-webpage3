eee4748ced4fe7e3daff0fb104a033e86e0e8bb8
shauryajain2067 <77152865+shauryajain2067@users.noreply.github.com>
2021-12-13 15:48:44 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAlnvuZ2BnsJ0oVlriNv4GOTS2rpRoH9lM",
+    authDomain: "quitter-b765e.firebaseapp.com",
+    databaseURL: "https://quitter-b765e-default-rtdb.firebaseio.com",
+    projectId: "quitter-b765e",
+    storageBucket: "quitter-b765e.appspot.com",
+    messagingSenderId: "446696722832",
+    appId: "1:446696722832:web:7977d555f31b75cb3b2315",
+    measurementId: "${config.measurementId}"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
